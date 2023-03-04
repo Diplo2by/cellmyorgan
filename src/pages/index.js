f8ffbf4bce835b675fdefd97c840b701dec69dff
Diplo2by <contactdarshanv@gmail.com>
2023-03-04 22:19:06 +0530

    recompiled artifacts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,12 @@
 import Test from '@/components/Test'
 import Head from 'next/head'
 
+import { organAddress, organListingAddress } from '../../config'
+import axios from 'axios'
+import web3modal from 'web3modal'
+
+import Organ from '../../artifacts/contracts/Organ.sol'
+import OrganListing from '../../artifacts/contracts/OrganListing.sol'
 
 export default function Home() {
   return (
@@ -10,6 +16,6 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Test></Test>
-      </>
+    </>
   )
 }
