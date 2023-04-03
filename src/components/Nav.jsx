2933fd5be5856c3499521e9e983f5cfcc2fe7b3f
Diplo2by <contactdarshanv@gmail.com>
2023-04-03 19:22:54 +0530

    Added contact page

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,15 +4,15 @@ import { redirect } from 'next/navigation';
 import Link from 'next/link';
 
 const Nav = () => {
-    let Links =[
-      {name:"HOME",link:"/"},
-      {name:"ABOUT",link:"/About"},
-      {name:"EVENTS",link:"/about2"},
-      {name:"GALLERY",link:"/"},
-      {name:"CONTACT",link:"/"},
-      {name:"FAQs",link:"/"},
-    ];
-    let [open,setOpen]=useState(false);
+  let Links = [
+    { name: "HOME", link: "/" },
+    { name: "ABOUT", link: "/about" },
+    { name: "EVENTS", link: "/" },
+    { name: "GALLERY", link: "/" },
+    { name: "CONTACT", link: "/contact" },
+    { name: "FAQs", link: "/" },
+  ];
+  let [open, setOpen] = useState(false);
   return (
     <div className='shadow-md w-full sticky top-0 left-0'>
       <div className='md:flex items-center justify-between bg-gray-400 py-4 md:px-10 px-7'>
@@ -24,24 +24,24 @@ const Nav = () => {
           <ion-icon name={open ? 'close' : 'menu'}></ion-icon>
         </div>
 
-      <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white-400 md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 ':'top-[-490px]'}`}>
-        {
-          Links.map((link)=>(
-            <li key={link.name} className='md:ml-8 text-xl md:my-0 my-7'>
-              <Link href={link.link} className='text-gray-800 hover:text-gray-200 duration-500'>{link.name}</Link>
-            </li>
-          ))
-        }
-        {/* <Button onClick={event =>  window.location.href='/register'}> */}
-        {/* </Button> */}
-        <button className='bg-gray-800 text-white py-2 px-6 rounded md:ml-8 hover:bg-gray-600 duration-200'>
-          <Link href='/register'>
-            register
-          </Link>
-          
-        </button>
+        <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white-400 md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 ' : 'top-[-490px]'}`}>
+          {
+            Links.map((link) => (
+              <li key={link.name} className='md:ml-8 text-xl md:my-0 my-7'>
+                <Link href={link.link} className='text-gray-800 hover:text-gray-200 duration-500'>{link.name}</Link>
+              </li>
+            ))
+          }
+          {/* <Button onClick={event =>  window.location.href='/register'}> */}
+          {/* </Button> */}
+          <button className='bg-gray-800 text-white py-2 px-6 rounded md:ml-8 hover:bg-gray-600 duration-200'>
+            <Link href='/register'>
+              register
+            </Link>
 
-      </ul>
+          </button>
+
+        </ul>
       </div>
     </div>
   )
