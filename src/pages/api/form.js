f1a267d7bf9686cd601393dfb4cb8c2b6a2245a5
Diplo2by <contactdarshanv@gmail.com>
2023-04-09 10:52:43 +0530

    added IPFS uploads

--- src/pages/api/form.js
+++ src/pages/api/form.js
@@ -1,11 +1,36 @@
 //WIP
+import { create as ipfsHttpClient } from 'ipfs-http-client'
 
-export default function handler(req, res) {
+const auth = 'Basic ' + Buffer.from(process.env.INFURA_PROJECT_ID + ':' + process.env.INFURA_API_KEY).toString('base64')
+
+const client = ipfsHttpClient({
+  host: 'ipfs.infura.io',
+  port: '5001',
+  protocol: 'https',
+  headers: {
+    authorization: auth,
+  },
+
+})
+async function ipfsPin(data) {
+  try {
+    const added = await client.add(JSON.stringify(data))
+    const url = process.env.IPFS_DEDICATED_SUBDOMAIN + added.path
+    return url
+  }
+  catch (e) {
+    console.log('There was an error uploading data:', e)
+  }
+
+}
+export default async function handler(req, res) {
   const body = req.body;
   if (req.method !== "POST") {
     res.status(504).json("only post allowed");
     return;
   }
-  console.log(body.name);
-  res.status(200).json({ data: "recieved", body: body });
+  const link = await ipfsPin(body)
+  // console.log(link)
+  res.status(200).json({ data: "recieved", body: body, url: link });
+
 }
