a7479e18985650eda3df27dfccdf97be4a575419
Samuel Jonathan <jonathandinesh@gmail.com>
2023-06-01 19:06:29 +0530

    Waiting list form 50%

---
+++ src/pages/doctor/index.js
@@ -0,0 +1,15 @@
+import WaitingListForm from "@/components/WaitingListForm";
+import Head from "next/head";
+
+function WaitingList() {
+  return (
+    <>
+      <Head>
+        <title>Registration Page</title>
+      </Head>
+      <WaitingListForm />
+    </>
+  );
+}
+
+export default WaitingList;
