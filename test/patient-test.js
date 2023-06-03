e9589bcd36c14c7bf6ecd01b48de3ef2353f1dba
Diplo2by <contactdarshanv@gmail.com>
2023-06-03 10:11:11 +0530

    patient listing test

--- test/patient-test.js
+++ test/patient-test.js
@@ -1,11 +1,19 @@
-describe.only('PatientListing', () => { it ("should list and retrieve patient details",async function () {
-    const Patient =await ethers.getContractFactory("Patient");
-    const pat = await Patient.deploy();
-    await pat.deployed()
-    const contractAddress = pat.address
-    console.log(contractAddress)
+describe.only('PatientListing', () => {
+    it("should list and retrieve patient details", async function () {
+        const Patient = await ethers.getContractFactory("Patient");
+        const pat = await Patient.deploy();
+        await pat.deployed()
+        const contractAddress = pat.address
+        console.log(contractAddress)
 
-    let count = await pat.getPatientCount();
-    console.log(Number(count));
-    
-}) })
+        await pat.listNewPatient(contractAddress, ".xyz", "Gopi", 69);
+        await pat.listNewPatient(contractAddress, ".xyz", "Gopal", 74);
+        await pat.listNewPatient(contractAddress, ".xyz", "Govind", 89);
+        let count = await pat.getPatientCount();
+        console.log(Number(count));
+
+        let val = await pat.getPatientAtIndex(2)
+        console.log(val)
+
+    })
+})
