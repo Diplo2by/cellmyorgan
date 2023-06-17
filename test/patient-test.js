68db9bb9387613e0786d5dcaae3030bb2af6d2bf
Diplo2by <contactdarshanv@gmail.com>
2023-06-17 15:08:17 +0530

    tests for contract

--- test/patient-test.js
+++ test/patient-test.js
@@ -1,4 +1,4 @@
-describe.only('PatientListing', () => {
+describe('PatientListing', () => {
     it("should list and retrieve patient details", async function () {
         const Patient = await ethers.getContractFactory("Patient");
         const pat = await Patient.deploy();
@@ -8,7 +8,9 @@ describe.only('PatientListing', () => {
 
         const [_,signer] = await ethers.getSigners()
         const senderAddress= await signer.address
-        await pat.listNewPatient(senderAddress, ".xyz", "Gopi", 69);
+        await pat.listNewPatient(senderAddress, ".xyz", "Gopi", 69,"liver");
+        await pat.listNewPatient(senderAddress, ".yzo", "Dinesh", 99,"Kidney");
+
         
         console.log(Number(await pat.getPatientCount()))
         console.log(await pat.getAllAddress())
