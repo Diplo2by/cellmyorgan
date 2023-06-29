09e31049ebf29aafcc86e2ea65d956e53d0dcd08
Samuel Jonathan <jonathandinesh@gmail.com>
2023-06-29 15:29:06 +0530

    hero centered

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,17 +13,17 @@ const Hero = () => {
         <h1 className="md:text-5xl sm:text-4xl text-3xl font-bold md:py-6">
           Welcome to Indian Organ Procurement and Transplantaion portal - IOPTN
         </h1>
-        <div className="text-gray-700 flex justify-center items-center w-max mr-12">
+        <div className="text-gray-700 flex justify-center items-center w-max relative -left-9">
           <p className="md:text-4xl sm:text-2xl text-xl font-bold py-4">
-            IOPTN has so far facilitated : 
+            IOPTN has so far facilitated :
           </p>
           <Typed
             className="md:text-4xl sm:text-2xl text-xl font-bold ml-2"
             strings={[
-              '100+ Procurements.',
-              '85+ Transplantations.',
-              '100+ Registered hospitals',
-              'Countless Happy Patients.'
+              "100+ Procurements.",
+              "85+ Transplantations.",
+              "100+ Hospitals",
+              "Countless Happy Patients.",
             ]}
             typeSpeed={120}
             backSpeed={140}
@@ -31,9 +31,11 @@ const Hero = () => {
           />
         </div>
         <p className="md:text-2xl text-xl font-bold text-gray-500 py-5">
-          Indian Organ Procurement and Transplantation Network is a unique public-private partnership that links all professionals involved in the Indian donation and transplantation system.
+          Indian Organ Procurement and Transplantation Network is a unique
+          public-private partnership that links all professionals involved in
+          the Indian donation and transplantation system.
         </p>
-        <Link href='/login'>
+        <Link href="/login">
           <button className="bg-gray-800 w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-white text-xl">
             Doctor Login
           </button>
