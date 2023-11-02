b798b5e96a5c76e78d18fdf267dd14e1e00c9677
Diplo2by <contactdarshanv@gmail.com>
2023-11-02 15:12:59 +0530

    uncommented config

---
+++ config.js
@@ -0,0 +1,3 @@
+export const organListingAddress = "0xa6EDB734394a29a922a43a2d2E395b299b3fAd79";
+export const organAddress = "0x578cd88C50F904DAe64279F95902985c16A5509A";
+export const patientAddress = "0x1aD466eA20951e569eB5Ba210FCEF6f462EEc495";
