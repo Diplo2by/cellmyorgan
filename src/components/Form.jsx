c3a76031c6d46d281880c2949e39ca073351cddc
Samuel Jonathan <jonathandinesh@gmail.com>
2023-11-23 16:33:37 +0530

    changed placeholder data on forms

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -396,7 +396,7 @@ const RegistrationForm = () => {
                   value={data.email}
                   className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                   type="text"
-                  placeholder="v@darsh.com"
+                  placeholder="alan@ioptn.com"
                 />
               </MainFormElement>
               <MainFormElement>
--- src/components/WaitingListForm.jsx
+++ src/components/WaitingListForm.jsx
@@ -522,7 +522,7 @@ const WaitingListForm = () => {
                     value={data.email}
                     className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                     type="text"
-                    placeholder="v@darsh.com"
+                    placeholder="alan@ioptn.com"
                   />
                 </div>
                 <div className="w-full md:w-1/3 px-3">
